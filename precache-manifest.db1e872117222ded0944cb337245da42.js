f82f450412b0449a101cd74e7421727538f4eaff
Satej Bidvai <satej.bidvai@gmail.com>
2020-07-12 22:15:30 +0530

    Updates

---
+++ precache-manifest.db1e872117222ded0944cb337245da42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ff28eaa058421450678bc27d2b69f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8665df7f8f2a3bd14968",
+    "url": "/static/css/main.0d369b9d.chunk.css"
+  },
+  {
+    "revision": "59a5e09756412d8d39a1",
+    "url": "/static/js/2.d43100c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d43100c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8665df7f8f2a3bd14968",
+    "url": "/static/js/main.f2dec3fe.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
